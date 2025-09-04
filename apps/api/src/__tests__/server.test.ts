9a3e1737a1436d722bd416ba93922c51f7a283a1
Andre Oliveira <aoliveira000@gmail.com>
2025-09-04 01:17:39 -0400

    toolpath

--- apps/api/src/__tests__/server.test.ts
+++ apps/api/src/__tests__/server.test.ts
@@ -1,23 +1,36 @@
 import supertest from "supertest";
 import { describe, it, expect } from "@jest/globals";
 import { createServer } from "../server";
+import { log } from "@repo/logger";
 
 describe("Server", () => {
-  it("health check returns 200", async () => {
-    await supertest(createServer())
-      .get("/status")
-      .expect(200)
-      .then((res) => {
-        expect(res.ok).toBe(true);
-      });
-  });
+    it("health check returns 200", async () => {
+        log({ message: "health check pinged." });
+        await supertest(createServer())
+            .get("/status")
+            .expect(200)
+            .then((res) => {
+                expect(res.ok).toBe(true);
+            });
+    });
 
-  it("message endpoint says hello", async () => {
-    await supertest(createServer())
-      .get("/message/jared")
-      .expect(200)
-      .then((res) => {
-        expect(res.body).toEqual({ message: "hello jared" });
-      });
-  });
+    it("GET tasks returns a list of tasks", async () => {
+        log({ message: "GET tasks pinged." });
+        await supertest(createServer())
+            .get("/tasks")
+            .expect(200)
+            .then((res) => {
+                expect(Array.isArray(res.body)).toEqual(true);
+            });
+    });
+
+    it("POST tasks returns a list of tasks", async () => {
+        log({ message: "POST tasks pinged." });
+        await supertest(createServer())
+            .post("/tasks")
+            .expect(200)
+            .then((res) => {
+                expect(res.body.acknowledged).toEqual(true);
+            });
+    });
 });
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,21 +2,42 @@ import { json, urlencoded } from "body-parser";
 import express, { type Express } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { Collection, MongoClient } from "mongodb";
+import { Task } from "@repo/types";
+
+const connectionString = "mongodb://localhost";
+const client = new MongoClient(connectionString);
+
+async function getCollection(): Promise<Collection> {
+    const conn = await client.connect();
+    const db = conn.db("tasks");
+    const collection = await db.collection("tasks");
+    return collection;
+}
 
 export const createServer = (): Express => {
-  const app = express();
-  app
-    .disable("x-powered-by")
-    .use(morgan("dev"))
-    .use(urlencoded({ extended: true }))
-    .use(json())
-    .use(cors())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
-    })
-    .get("/status", (_, res) => {
-      return res.json({ ok: true });
-    });
+    const app = express();
+    app.disable("x-powered-by")
+        .use(morgan("dev"))
+        .use(urlencoded({ extended: true }))
+        .use(json())
+        .use(cors())
+        .get("/status", async (_, res) => {
+            return res.json({ ok: true });
+        })
+        .get("/tasks", async (req, res) => {
+            const collection = await getCollection();
+            const results = await collection.find({}).toArray();
+            client.close();
+            return res.json(results);
+        })
+        .post("/tasks", async (req, res) => {
+            const collection = await getCollection();
+            const task: Task = req.body;
+            const result = await collection.insertOne(task);
+            client.close();
+            return res.json(result);
+        });
 
-  return app;
+    return app;
 };
